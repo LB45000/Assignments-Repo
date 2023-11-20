db54aab79bcf801ae17dfd0c64ba15a379f710e0
LB45000 <144392042+LB45000@users.noreply.github.com>
2023-11-19 17:42:47 -0800

    Create JavaScriptClasses Exercise#1(TSB).js

---
+++ JavaScriptClasses Exercise#1(TSB).js
@@ -0,0 +1,11 @@
+class Student {
+    constructor(name, age, major) {
+        this.name = name;
+        this.age = age;
+        this.major = major;
+    }
+}
+
+const student1 = new Student("John Doe", 20, "Computer Science");
+
+console.log(student1);
